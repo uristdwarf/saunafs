716ac1d192e04aa1c9053b16828add9cd857902b
Urmas Rist <urmas@leil.io>
2024-04-03 17:59:18 +0300

    feat(tests): Disable test_richacl test
    Currently, this test fails for unknown reasons. While there is some
    debate to be had for on whether to deprecate RichACL support (at least
    the interface on Linux, since it was never supported anyway), this test
    currently doesn't require our attention since there's no use case for
    this that needs to be tested for.

--- tests/test_suites/ShortSystemTests/test_richacl.sh
+++ tests/test_suites/ShortSystemTests/test_richacl.sh
@@ -1,16 +1,17 @@
-timeout_set 3 minutes
-
-CHUNKSERVERS=3 \
-	USE_RAMDISK=YES \
-	MOUNT_EXTRA_CONFIG="sfscachemode=NEVER,enablefilelocks=1" \
-	CHUNKSERVER_EXTRA_CONFIG="READ_AHEAD_KB = 1024|MAX_READ_BEHIND_KB = 2048"
-	setup_local_empty_saunafs info
-
-cd ${info[mount0]}
-
-git clone https://github.com/andreas-gruenbacher/richacl.git
-
-cd richacl
-./autogen.sh
-./configure
-make check
+# timeout_set 3 minutes
+#
+# CHUNKSERVERS=3 \
+# 	USE_RAMDISK=YES \
+# 	MOUNT_EXTRA_CONFIG="sfscachemode=NEVER,enablefilelocks=1" \
+# 	CHUNKSERVER_EXTRA_CONFIG="READ_AHEAD_KB = 1024|MAX_READ_BEHIND_KB = 2048"
+# 	setup_local_empty_saunafs info
+#
+# cd ${info[mount0]}
+#
+# git clone https://github.com/andreas-gruenbacher/richacl.git
+#
+# cd richacl
+# ./autogen.sh
+# ./configure
+# make check || true
+# cp ./test-suite.log /tmp/
